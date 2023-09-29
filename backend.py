402ee45d0768b8e34950f56ce5ed9cd0efdced16
Arpan Swain <kankalase07@gmail.com>
2023-09-29 16:28:51 +0530

    Connected front end to backend to display results

--- backend.py
+++ backend.py
@@ -2,19 +2,14 @@ import requests
 
 api_key = "141710af2113bab9f55ef73e1bcd33d5"
 
-def get_data(place,forecastday=None,kind=None):
+def get_data(place,forecastday=None):
     url=f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8*forecastday
     filtered_data=filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data=[x["main"]["temp"] for x in filtered_data]
-    elif kind == "Sky":
-        filtered_data = [x["weather"][0]["main"] for x in filtered_data]
-
     return filtered_data
 
 if __name__ == "__main__":
-    print(get_data("Delhi",forecastday=2,kind="Temperature"))
+    print(get_data("Delhi",forecastday=2))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for Next Days")
 
@@ -9,6 +10,17 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view",
                       ("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
+if place:
+    filtered_data = get_data(place,days)
+    if option == "Temperature":
+        temperature = [int(x["main"]["temp"])/10 for x in filtered_data]
+        dates = [x["dt_txt"] for x in filtered_data]
+        figure = px.line(x=dates,y=temperature, labels={"x":"Date","y":"Temperature(C)"})
+        st.plotly_chart(figure)
 
-figure = px.line(x=[1,2,3],y=[4,5,6], labels={"x":"Date","y":"Temperature(C)"})
-st.plotly_chart(figure)
+    if option == "Sky":
+        stored_img = {"Clear":"images/clear.png","Clouds":"images/cloud.png","Rain":"images/rain.png",
+                      "Snow":"images/snow.png"}
+        sky_conditions = [x["weather"][0]["main"] for x in filtered_data]
+        images = [stored_img[x] for x in sky_conditions]
+        st.image(images, width=110)

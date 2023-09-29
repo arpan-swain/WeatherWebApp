13628eac779ece9e48bafd0ea7bea8cfbb535708
Arpan Swain <kankalase07@gmail.com>
2023-09-29 16:01:59 +0530

    Designed Backend to fetch data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather Forecast for Next Days")
 
@@ -8,3 +9,6 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view",
                       ("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
+
+figure = px.line(x=[1,2,3],y=[4,5,6], labels={"x":"Date","y":"Temperature(C)"})
+st.plotly_chart(figure)

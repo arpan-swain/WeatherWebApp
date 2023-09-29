1a128c9e2b31f0bb48da4c8cb0f10d728d8e66d1
Arpan Swain <kankalase07@gmail.com>
2023-09-29 16:31:39 +0530

    Fixed incorrect temperature and invalid city issue

--- main.py
+++ main.py
@@ -10,17 +10,20 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view",
                       ("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
-if place:
-    filtered_data = get_data(place,days)
-    if option == "Temperature":
-        temperature = [int(x["main"]["temp"])/10 for x in filtered_data]
-        dates = [x["dt_txt"] for x in filtered_data]
-        figure = px.line(x=dates,y=temperature, labels={"x":"Date","y":"Temperature(C)"})
-        st.plotly_chart(figure)
+try:
+    if place:
+        filtered_data = get_data(place,days)
+        if option == "Temperature":
+            temperature = [int(x["main"]["temp"])/10 for x in filtered_data]
+            dates = [x["dt_txt"] for x in filtered_data]
+            figure = px.line(x=dates,y=temperature, labels={"x":"Date","y":"Temperature(C)"})
+            st.plotly_chart(figure)
 
-    if option == "Sky":
-        stored_img = {"Clear":"images/clear.png","Clouds":"images/cloud.png","Rain":"images/rain.png",
-                      "Snow":"images/snow.png"}
-        sky_conditions = [x["weather"][0]["main"] for x in filtered_data]
-        images = [stored_img[x] for x in sky_conditions]
-        st.image(images, width=110)
+        if option == "Sky":
+            stored_img = {"Clear":"images/clear.png","Clouds":"images/cloud.png","Rain":"images/rain.png",
+                          "Snow":"images/snow.png"}
+            sky_conditions = [x["weather"][0]["main"] for x in filtered_data]
+            images = [stored_img[x] for x in sky_conditions]
+            st.image(images, width=110)
+except KeyError:
+    st.write("We did not found any data for that city !")
